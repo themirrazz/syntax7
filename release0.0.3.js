71df96dec0996e1d406ab98bac5af8ab484aa02c
themirrazz <89490659+themirrazz@users.noreply.github.com>
2022-05-23 20:56:06 -0700

    Create release0.0.3.js

---
+++ release0.0.3.js
@@ -0,0 +1,33 @@
+# python works too!
+import random
+from turtle import Pen
+import i_am_a_python_module_that_is_not_useful as notuseful
+mynumber = 1234567890
+# oh no i lost my phone number
+mynumber = None
+yes = True
+no = False
+
+while True:
+  print("OH MY GOSH THIS ACTUALLY WORKS??")
+  print("I'm gonna make a function now...")
+  break
+
+def bruh():
+  import mom # yes, this works (specificaly in brython)
+  mom.scream_at("BRUUUUUUUUUUUUUUUUUUH")
+  '''
+  my mom hates the word "bruh"
+  python has no multiline comments
+  so i use strings instead
+  this feels stretchy
+  \''' ha you thought the highlighting was that bad?
+  '''
+
+if True:
+  pass # i forgot what to put here.
+
+class Math:
+  def getAnFYI():
+    print("PAY ATTENTION TO THESE RANDOM LINEAR EQUATIONS!")
+    print("You have one FYI. Get 3, thats DETENTION")
